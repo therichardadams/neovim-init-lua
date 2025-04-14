4c99f444af48274fb03ab23271fdadc8509a2311
Richard Adams <dev@radams.io>
2025-04-14 21:34:01 +0100

    LSP & cmp config cleanup

--- lua/radams/set.lua
+++ lua/radams/set.lua
@@ -51,3 +51,11 @@ vim.api.nvim_create_autocmd({"Filetype"}, {
     pattern = {"lua"},
     command = "setlocal tabstop=2 shiftwidth=2 softtabstop=2"
 })
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = vim.api.nvim_create_augroup("help_window_right", {}),
+    pattern = { "*.txt" },
+    callback = function()
+        if vim.o.filetype == 'help' then vim.cmd.wincmd("L") end
+    end
+})
